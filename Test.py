0558083970f4325a63ecec81f09610fb556c15eb
KacperSob <109038663+KacperSob@users.noreply.github.com>
2023-01-02 20:53:41 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,2 +1,16 @@
-if __name__ == '__main__':
-	print("Hello World")
+n = 2
+ktora = 1
+while (True):
+	flag = 0
+	i = 2
+	while i <= (n/2):
+	    if (n%i) == 0:
+	        flag = 1
+	        break
+	    i += 1
+	if n == 1:
+		print("error")
+	elif flag == 0:
+	    print(n, "to ", ktora, " liczba pierwsza")
+	    ktora += 1
+	n += 1
